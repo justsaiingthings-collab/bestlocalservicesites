533eb405347ba52a277650a5401d1d73f5bc9c9f
justsaiing <justsaiingthings@gmail.com>
2026-04-27 10:38:39 -0700

    chore: add out/ to .gitignore, stop tracking build artifacts

--- apps/landing/out/_next/static/chunks/app/layout-cff5c8f16dda66fb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1539:function(n,e,t){Promise.resolve().then(t.t.bind(t,190,23)),Promise.resolve().then(t.t.bind(t,9463,23))},190:function(){}},function(n){n.O(0,[754,463,530,801,744],function(){return n(n.s=1539)}),_N_E=n.O()}]);
